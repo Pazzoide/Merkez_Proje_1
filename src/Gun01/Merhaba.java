d48450406ace89cc0761cf9b8785596a32a2ab69
BURHAN <Brhn.Sngl.95@outlook.com>
2023-08-07 22:18:07 +0300

    Proje güncellendi! Yeni kodlar eklendi!

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,5 +3,11 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Burhan");
+        System.out.println("Fatih");
+        System.out.println("Burcu");
+        System.out.println("Engin");
+        System.out.println("Zafer");
+        System.out.println("Ramazan");
     }
 }
